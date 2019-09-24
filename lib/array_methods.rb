da9c4141b41a3d0c2149c9539835b0b434e71bd9
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-24 01:52:07 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -3,7 +3,8 @@ def using_include(array, element)
 end
 
 def using_sort(array)
- array = ["wow", "i", "am", "really", "learning", "arrays"]
+ array = ["wow", "i", "am", "really", "learning", "arrays!"]
+ array.sort
 end
 
 def using_reverse(array)
